b3cdffc9ca0427b184d308516a383385078b250f
Domin C <dominikcedro00@gmail.com>
2023-12-21 00:09:05 +0100

    Created directory for classes used in recycler_view_request.xml, basic  layout to showcase function. Now implementing firestore option for that

---
+++ app/src/main/java/com/example/projectmanager/RequestsDayRecycler/Requests_RecyclerViewAdapter.kt
@@ -0,0 +1,39 @@
+package com.example.projectmanager.RequestsDayRecycler
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.example.projectmanager.R
+
+class Requests_RecyclerViewAdapter(var dataSet: ArrayList<RequestModel>) :
+    RecyclerView.Adapter<Requests_RecyclerViewAdapter.ViewHolder>() {
+
+        class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+            val requestAuthor: TextView = view.findViewById(R.id.tvAuthor)
+            val requestSubject: TextView = view.findViewById(R.id.tvSubject)
+            val requestStartDate: TextView = view.findViewById(R.id.tvRequestStartt)
+            val requestEndDate: TextView = view.findViewById(R.id.tvRequestEnd)
+            val requestTotalTime: TextView = view.findViewById(R.id.tvTotalTime)
+        }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val view = LayoutInflater.from(parent.context)
+            .inflate(R.layout.recycler_view_row, parent, false)
+        return ViewHolder(view)
+    }
+
+    override fun getItemCount(): Int {
+        return dataSet.size
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        val request = dataSet[position]
+        holder.requestAuthor.text = request.username
+        holder.requestSubject.text = request.subject
+        holder.requestStartDate.text = request.startDate
+        holder.requestEndDate.text = request.endDate
+        holder.requestTotalTime.text = request.totalTime
+    }
+}
